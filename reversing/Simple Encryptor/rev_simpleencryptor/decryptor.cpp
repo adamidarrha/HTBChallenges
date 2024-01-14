a2b01fe4cd771639adb99ce6a07470440fa0a524
adamIdarrha <adamidarrha@gmail.com>
2024-01-14 17:39:06 +0100

    d

---
+++ reversing/Simple Encryptor/rev_simpleencryptor/decryptor.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *file = fopen("flag.enc", "rb");
+    if (file == NULL) {
+        printf("Could not open file flag.enc\n");
+        return 1;
+    }
+
+    unsigned int seed;
+    size_t bytesRead = fread(&seed, sizeof(unsigned int), 1, file);
+    if (bytesRead != 1) {
+        printf("Could not read seed from file\n");
+        return 1;
+    }
+
+    srand(seed);
+
+    // Get the file size
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file) - sizeof(unsigned int);
+    fseek(file, sizeof(unsigned int), SEEK_SET);
+
+    // Allocate memory for the file data
+    unsigned char *fileDataPointer = (unsigned char *)malloc(fileSize);
+    if (fileDataPointer == NULL) {
+        printf("Could not allocate memory for file data\n");
+        return 1;
+    }
+
+    // Read the file data
+    bytesRead = fread(fileDataPointer, 1, fileSize, file);
+    if (bytesRead != fileSize) {
+        printf("Could not read file data\n");
+        return 1;
+    }
+
+    for (int i = 0; i < fileSize; i++) {
+        unsigned char firstRandomNumber = rand();
+        fileDataPointer[i] = fileDataPointer[i] ^ firstRandomNumber;
+        unsigned int secondRandomNumber = rand();
+        secondRandomNumber = secondRandomNumber & 7;
+        fileDataPointer[i] = (fileDataPointer[i] << secondRandomNumber) | (fileDataPointer[i] >> (8 - secondRandomNumber));
+    }
+
+    free(fileDataPointer);
+    fclose(file);
+    return 0;
+}
